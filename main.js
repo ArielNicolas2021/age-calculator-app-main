2847f37cc9bc2cfeb23824bb92ad81804d5e0c4a
Ariel <ariel.nicolas.aguilar@gmail.com>
2024-03-18 09:31:41 -0300

    Fix error validate

--- main.js
+++ main.js
@@ -1,36 +1,41 @@
 let yearIn = document.getElementById('yearInput')
 let yearOut = document.getElementById('yearOutput')
 let yearTime = new Date().getFullYear()
+let yearLabel = document.getElementById('yearLabel')
+
 let monthIn = document.getElementById('monthInput')
 let monthOut = document.getElementById('monthOutput')
 let monthTime = new Date().getMonth() + 1
+let monthLabel = document.getElementById('monthLabel')
+
 let dayIn = document.getElementById('dayInput')
 let dayOut = document.getElementById('dayOutput')
 let dayTime = new Date().getDate()
-let days = ''
+let dayLabel = document.getElementById('dayLabel')
 
+let days = ''
 let error = document.getElementById('error')
+
 
 document.getElementById('btn').addEventListener('click', verification)
 
 // Detect leap years
-if (monthTime === 1 || monthTime === 3 || monthTime === 5 || monthTime === 7 || monthTime === 8 || monthTime === 10 || monthTime === 12 ) {
-    days = 31
-} else if (monthTime === 2 && monthTime % 4 === 0) {
-    days = 29
-} else if (monthTime === 2) {
-    days = 28
-} else {
-    days = 30
-}
+
 
 // Check if the inputs are correct
 function verification(e) {
     e.preventDefault()
+
     if(dayIn.value === '' || monthIn.value === '' || yearIn.value === '' || dayIn.value <= 0 || dayIn.value > 31 || monthIn.value <= 0 || monthIn.value > 12 || yearIn.value < 1900 || yearIn.value > yearTime) {
         dayIn.classList.add('error-input')
+        dayLabel.style.color = 'var(--light-red)'
+
         monthIn.classList.add('error-input')
+        monthLabel.style.color = 'var(--light-red)'
+
         yearIn.classList.add('error-input')
+        yearLabel.style.color = 'var(--light-red)'
+
         error.style.display = 'block'
     } else {
         calculate()
@@ -39,6 +44,16 @@ function verification(e) {
 
 // Calculate your age
 function calculate() {
+    if (monthIn.value === 1 || monthIn.value === 3 || monthIn.value === 5 || monthIn.value === 7 || monthIn.value === 8 || monthIn.value === 10 || monthIn.value === 12 ) {
+        days = 31
+    } else if (monthIn.value === 2 && yearIn.value % 4 === 0) {
+        days = 29
+    } else if (monthIn.value === 2) {
+        days = 28
+    } else {
+        days = 30
+    }
+
     if (monthTime - monthIn.value < 0 && dayTime - dayIn.value < 0) {
         yearOut.innerHTML = `${yearTime - yearIn.value - 1}<span> years</span>`
         monthOut.innerHTML = `${monthTime - monthIn.value + 11}<span> months</span>`
@@ -62,4 +77,10 @@ function calculate() {
         monthOut.innerHTML = `${monthTime - monthIn.value}<span> months</span>`
         dayOut.innerHTML = `${dayTime - dayIn.value}<span> days</span>`
     }
+
+    dayIn.classList.remove('error-input')
+    monthIn.classList.remove('error-input')
+    yearIn.classList.remove('error-input')
+    error.style.display = 'none'
+
 }
